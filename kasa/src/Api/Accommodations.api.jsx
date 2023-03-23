97b3ef1a5e5101583337923aaf4604e53930126e
fvnny-code <f.codecco.grando@gmail.com>
2023-03-23 11:55:35 +0100

    comment of async function getAccommodations : a maping of the products in data files is enough for this case.

--- kasa/src/Api/Accommodations.api.jsx
+++ kasa/src/Api/Accommodations.api.jsx
@@ -1,13 +1,13 @@
 import products from "../Data/accommodations.json";
 
 
-export async function getAccommodationById(id){
+export async function getAccommodationById(id) {
 
     const foundProduct = products.find((p) => p.id === id);
     return foundProduct;
 
 
 }
-export async function getAccommodations (){
-return products;
-}
+// export async function getAccommodations (){
+// return products;
+// }
--- kasa/src/Components/Cards/Card.jsx
+++ kasa/src/Components/Cards/Card.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
-import { AccommodationList } from '../../Data/AccommodationList';
+
+import Data from '../../Data/accommodations.json'
 import './card.css'
 
-export default function Card() { 
-   
+export default function Card() {
+
     return (
         <main className="accommodation-section">
             <div className="accommodation-container">
-                {AccommodationList.map(({ id, cover, title }) =>
+
+                {Data.map(({ id, cover, title }) =>
                     <article key={id} className="card-accommodation__content">
                         <Link to={`/location/${id}`}>
                             <div className="card-accommodation__bg"></div>
-                            <img src={cover} alt="logement en location" className="card-accommodation__cover"/>
+                            <img src={cover} alt="logement en location" className="card-accommodation__cover" />
                             <p className="card-accommodation__title">{title}</p>
                         </Link>
 
