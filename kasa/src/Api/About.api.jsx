27f9ba52e194d4518171968859b082d9ae5c6e8e
fvnny-code <f.codecco.grando@gmail.com>
2023-03-23 11:38:00 +0100

    deletion of About.api.jsx : overkill for this project.

--- kasa/src/Api/About.api.jsx
+++
@@ -1,6 +0,0 @@
-import AboutItems from '../../Data/AboutItems.json' // json file created with collapses data
-
-export async function getAboutItemsById(id) {
-    const foundItems = items.find((i) => i.id === id);
-    return foundItems;
-}
